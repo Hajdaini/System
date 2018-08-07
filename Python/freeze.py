008ff8830a403168917fd1b09a7751a7c9e1903b
jhechavarria <jessie.hechavarria@gmail.com>
2018-08-07 03:08:53 +0200

    Definition automatique du PATH associe au script d'empaquetage

--- Python/freeze.py
+++ Python/freeze.py
@@ -1,8 +1,11 @@
+import os
 from cx_Freeze import setup, Executable
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 setup(
     name = "ftp",
     version = "0.0.1",
     description = "Script de gestion FTP",
-    executables = [Executable("Python/main.py")]
+    executables = [Executable("{}/main.py".format(dir_path))]
 )
