649f0224b6d6e676753c7c8e38555843c005007c
hajdaini <ajdaini.hatim@gmail.com>
2018-08-09 16:50:59 +0200

    Fix Abosolute Path bug for cat, get and wc commands

--- Python/ftp/commands/cat.py
+++ Python/ftp/commands/cat.py
@@ -3,21 +3,20 @@
 import os
 from commands.Command import Command
 from modules.color import error
+import random, string
 
 class cat(Command):
     def __init__(self, args, ftp, address, user):
         Command.__init__(self, args, ftp, address, user)
 
     def call(self):
-        #try:
-        filename = self.ftp.abspath(self.argv[1])
-        print(filename)
-        """
-        with open(filename, 'wb') as file:
-            self.ftp.retrbinary('RETR %s' % filename, file.write)
-        with open(filename, 'r') as file:
-            print(file.read())
-        os.remove(filename)
-        """
-        #except:
-            #error('File may not exist or you may not have permission to access it.')
+        try:
+            random_filename = (''.join(random.choice(string.ascii_lowercase) for _ in range(6)))
+            filename = self.argv[1]
+            with open(random_filename, 'wb') as file:
+                self.ftp.retrbinary('RETR %s' % filename, file.write)
+            with open(random_filename, 'r') as file:
+                print(file.read())
+            os.remove(random_filename)
+        except:
+            error('File may not exist or you may not have permission to access it.')
