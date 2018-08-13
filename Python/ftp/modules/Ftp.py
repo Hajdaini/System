e6be3f98cc6130c5bd3cdba0ef5da2dfdecf25fd
jhechavarria <jessie.hechavarria@gmail.com>
2018-08-13 17:52:30 +0200

    ok pull

--- Python/ftp/modules/Ftp.py
+++ Python/ftp/modules/Ftp.py
@@ -4,7 +4,6 @@ import io, re, os
 from modules.color import *
 from ftplib import FTP
 from pathlib import Path
-from modules.path import *
 from modules.Capture import Capture
 
 class Ftp(FTP):
@@ -12,11 +11,11 @@ class Ftp(FTP):
         FTP.__init__(self, host, timeout=timeout)
         self.home = None
 
-    def is_empty(self, path):
+    def is_empty(self, path="./"):
         """
         Verifie si un dossier distant est vide
         """
-        path = self.abspath(path)
+        path = self.sabspath(path)
         if not self.exists(path) or self.is_file(path):
             return False
         with Capture() as output:
@@ -27,52 +26,53 @@ class Ftp(FTP):
         """
         Verifie si le fichier distant est un dossier
         """
-        path = self.abspath(path)
+        path = self.sabspath(path)
         if len(path) == 1 and path[0] == "/":
             return True
         test = path.split("/")
         test = test[len(test) - 1]
-        path = abspath(path, "../")
+        path = self.abspath(path, "../")
         ls = self.ls_info(path)
         return test in ls and ls[test]["type"] == "dir"
 
-    def is_file(self, path):
+    def is_file(self, path="./"):
         """
         Verifie si le fichier distant est un fichier regulier
         """
-        path = self.abspath(path)
+        path = self.sabspath(path)
         if len(path) == 1 and path[0] == "/":
             return False
         test = path.split("/")
         test = test[len(test) - 1]
-        path = abspath(path, "../")
+        path = self.abspath(path, "../")
         ls = self.ls_info(path)
         return test in ls and ls[test]["type"] == "file"
 
-    def exists(self, path):
+    def exists(self, path="./"):
         """
         Verifieifie si le fichier distant existe
         """
-        path = self.abspath(path)
+        path = self.sabspath(path)
         if len(path) == 1 and path[0] == "/":
             return False
         test = path.split("/")
         test = test[len(test) - 1]
-        path = abspath(path, "../")
+        path = self.abspath(path, "../")
         ls = self.ls_info(path)
         return test in ls
 
-    def pull(self, srcpath, destpath=None, pverwrite=False):
+    def pull(self, srcpath="./", destpath=None, overwrite=False):
         """
         Telecharge un fichier du serveur
         """
         if destpath == None:
-            destpath = srcpath
-        srcpath = self.abspath(srcpath)
-        destpath = cabspath(destpath)
+            destpath = srcpath.splis("/")[-1]
+        srcpath = self.sabspath(srcpath)
+        destpath = self.cabspath(destpath)
         if not overwrite and Path(destpath).exists():
-            warning("Local file already exists: " + destpath)
-            return
+            return warning("Local file already exists: " + destpath)
+        elif self.is_dir(srcpath):
+            return warning("Source file is a directory: " + srcpath)
         try:
             destfile = open(destpath, "wb")
             try:
@@ -83,7 +83,7 @@ class Ftp(FTP):
         except:
             error("File transfer failed: " + srcpath)
 
-    def push(self, srcpath, destpath=None, overwrite=False):
+    def push(self, srcpath="./", destpath=None, overwrite=False):
         """
         Envoie un fichier au serveur
         """
@@ -104,7 +104,7 @@ class Ftp(FTP):
         except:
             error("File transfer failed: " + srcpath)
 
-    def pullr(self, srcpath, destpath=None, pverwrite=False):
+    def pullr(self, srcpath="./", destpath=None, overwrite=False):
         """
         Telecharge une arborescence de fichiers du serveur
         """
@@ -128,7 +128,7 @@ class Ftp(FTP):
                 else:
                     self.pushr(src, dest, overwrite)
 
-    def pushr(self, srcpath, destpath=None, overwrite=False):
+    def pushr(self, srcpath="./", destpath=None, overwrite=False):
         """
         Envoie une arborescence de fichiers au serveur
         """
@@ -152,17 +152,55 @@ class Ftp(FTP):
                 else:
                     self.pushr(src, dest, overwrite)
 
-    def abspath(self, path):
+    def cabspath(self, path):
         """
-        Determine un chemin absolu a partir d'un chemin quelconque
+        Recompose un chemin absolu a partir de la position sur le terminal client
         """
-        return sabspath(self, path)
+        if path[0] == "~":
+            return str(Path.home()) + path[1:]
+        try:
+            pwd = str(Path.home())
+            return self.abspath(pwd, path)
+        except:
+            return None
 
-    def ls_info(self, path):
+    def sabspath(self, path):
+        """
+        Recompose un chemin absolu a partir de la position sur le yrtminal serveur
+        """
+        if path[0] == "~":
+            return self.home + path[1:]
+        try:
+            pwd = self.pwd()
+            return self.abspath(pwd, path)
+        except:
+            return None
+
+    def abspath(self, pwd, path):
+        """
+        Recompose un chemin absolu a partir de deux chaines quelconques
+        """
+        if path[0] == "/":
+            return path
+        pwd = pwd.split("/")
+        del pwd[0]
+        cpath = path.split("/")
+        for idx, el in enumerate(path.split("/")):
+            if el == ".." or el == ".":
+                del cpath[0]
+                if el == ".." and len(pwd):
+                    pwd.pop()
+            else:
+                break
+        if (len(pwd)):
+            pwd[0] = "/{}".format(pwd[0])
+        return "{}/{}".format("/".join(pwd), "/".join(cpath))
+
+    def ls_info(self, path="./"):
         """
         Recupere les donnees de chaque entree de la liste
         """
-        path = self.abspath(path)
+        path = self.sabspath(path)
         info = {}
         with Capture() as output:
             self.dir(path)
