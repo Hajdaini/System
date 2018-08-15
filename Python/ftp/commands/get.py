ac8f9da15d1af59e08c079821d058fbb7a329d37
hajdaini <ajdaini.hatim@gmail.com>
2018-08-15 13:03:40 +0200

    performance test

--- Python/ftp/commands/get.py
+++ Python/ftp/commands/get.py
@@ -1,25 +1,14 @@
 # coding:utf-8
 
 from modules.Command import Command
-from modules.color import error, success, info
-import random, string, os
-
+from modules.color import error
+import time
 
 class get(Command):
     def __init__(self, args, ftp):
         Command.__init__(self, args, ftp)
 
     def call(self):
-        try:
-            file = "{}/{}".format(self.ftp.pwd(), self.argv[1])
-            info("Downloading ...")
-            if self.ftp.is_dir(file):
-                self.ftp.download_tree(file)
-            else:
-                random_filename = (''.join(random.choice(string.ascii_lowercase) for _ in range(6)))
-                self.ftp.retrbinary('RETR ' + self.argv[1], open(random_filename, 'wb').write)
-                real_filename = self.argv[1].replace("\\", "/").split("/")[-1]
-                os.rename(random_filename, real_filename)
-            success('Download success !\n')
-        except:
-            error('File may not exist or you may not have permission to view it.')
+        start_time = time.time()
+        self.ftp.pull(self.argv[1], self.argv[2])
+        print("Finish in {0:.4f} s".format(time.time() - start_time))
