9b8d1bae042090ab4fc2d70891fe67cb3dbe0e6a
Hatim <38246876+hajdaini@users.noreply.github.com>
2020-01-31 23:08:18 +0100

    Support Ubuntu in emojis script

--- Shell/automatic-update/fonts-emojis/font-emoji.sh
+++ Shell/automatic-update/fonts-emojis/font-emoji.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-sudo dnf install google-noto-emoji-color-fonts
-
+os=`lsb_release -d | awk -F"\t" '{print $2}'`
 config_location="~/.config/fontconfig/"
 
 mkdir -p $config_location
 
-mv fonts.conf $config_location
+
+if echo ${os} | grep -i 'ubuntu\|debian'; then
+    sudo apt install fonts-noto-color-emoji
+    mv fonts-ubuntu.conf ${config_location}/fonts.conf
+else
+    sudo dnf install google-noto-emoji-color-fonts
+    mv fonts-fedora.conf ${config_location}/fonts.conf
+fi
+
 
 fc-cache -f
